5c1750f99f858d7e44b907a22fea968609f45e7e
jefrryss <y2p00t6@gmail.com>
2026-04-04 19:46:19 +0300

    main + compute

--- cmd/main.go
+++ cmd/main.go
@@ -1,5 +1,70 @@
 package main
 
-func main(){
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strings"
+
+	"go.uber.org/zap"
+
+	"in-memory/internal/compute"
+	"in-memory/internal/compute/parser"
+	"in-memory/internal/storage"
+	"in-memory/internal/storage/engine"
+)
+
+func InitLogger() *zap.Logger {
+	config := zap.NewDevelopmentConfig()
+
+	config.OutputPaths = []string{"db.log"}
+	config.ErrorOutputPaths = []string{"db.log"}
+
+	logger, err := config.Build()
+	if err != nil {
+		panic(err)
+	}
+
+	return logger
+}
+
+func main() {
+	logger := InitLogger()
+	defer logger.Sync()
 	
+	logger.Info("start init logs")
+	
+	prs := parser.NewLineParser()
+	mem := engine.NewMemory() 
+	store := storage.NewStorage(mem)
+	
+	comp := compute.NewCompute(prs, store, logger)
+	
+	logger.Info("start init db")
+	
+	scanner := bufio.NewScanner(os.Stdin)
+	for {
+		fmt.Print("> ")
+
+		if !scanner.Scan() {
+			break
+		}
+		
+		line := strings.TrimSpace(scanner.Text())
+		if line == "" {
+			continue
+		}
+
+		if line == "exit" {
+			logger.Info("end of work db")
+			return
+		}
+		
+		result, err := comp.HandleQuery(line)
+		if err != nil {
+			fmt.Printf("Error: %v\n", err)
+		} else {
+			fmt.Printf("Result: %s\n", result)
+		}
+	}
 }
--- internal/compute/compute.go
+++ internal/compute/compute.go
@@ -1,26 +1,64 @@
 package compute
 
-import(
+import (
+	"errors"
+
+	"go.uber.org/zap"
+
 	"in-memory/internal/compute/parser"
-	"in-memory/internal/compute/storage"
+	"in-memory/internal/storage"
 )
 
 type Compute struct {
-	parser *parser.Parser
-	storage *storage.Storage
-	loger *zap.Logger
+	parser  parser.Parser
+	storage storage.Storage
+	logger  *zap.Logger 
 }
 
-func NewCompute(p parser.Parser, s storage.Storage, l *zap.Logger) *Compute{
+func NewCompute(p parser.Parser, s storage.Storage, l *zap.Logger) *Compute {
 	return &Compute{
-		parser: p,
+		parser:  p,
 		storage: s,
-		loger: l,
+		logger:  l,
 	}
 }
 
-func (c *Compute) HandleRead(query string) error {
+func (c *Compute) HandleQuery(queryStr string) (string, error) {
+	c.logger.Debug("start parsing", zap.String("query", queryStr))
+	
+	query, err := c.parser.Parse(queryStr)
+	if err != nil {
+		c.logger.Warn("Invalid query syntax", zap.Error(err), zap.String("query", queryStr))
+		return "", err
+	}
 
+	switch query.Command {
+	case "SET":
+		err := c.storage.Set(query.Key, query.Value)
+		if err != nil {
+			c.logger.Error("failed SET", zap.Error(err), zap.String("key", query.Key))
+			return "", err
+		}
+		return "ok", nil
+
+	case "GET":
+		value, err := c.storage.Get(query.Key)
+		if err != nil {
+			c.logger.Info("failed GET (key not found)", zap.Error(err), zap.String("key", query.Key))
+			return "", err
+		}
+		return value, nil
+
+	case "DEL":
+		err := c.storage.Del(query.Key)
+		if err != nil {
+			c.logger.Error("failed DEL", zap.Error(err), zap.String("key", query.Key))
+			return "", err
+		}
+		return "ok", nil
+
+	default:
+		c.logger.Error("unknown command passed parser", zap.String("command", query.Command))
+		return "", errors.New("internal error: unknown command")
+	}
 }
-
-
